32f49e9dc301d5cc41e2ea38d6b64fd0996cdc28
Francis <nilesh1234nd@gmail.com>
2017-11-13 16:54:04 +0530

    Add files via upload

---
+++ WeatherDataDTO.java
@@ -0,0 +1,58 @@
+package com.app.myapplication.model;
+
+/**
+ * Created by Nilesh on 8/16/2017.
+ */
+
+public class WeatherDataDTO {
+
+    public String cityId;
+
+    public String city;
+
+    public String temp;
+
+    public String pressure;
+
+    public String humidity;
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+}
